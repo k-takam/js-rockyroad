981872a8a903b13754a25a55e622da46385cfc50
k-takam <takamatsu@tsumikiinc.com>
2015-10-14 19:28:07 +0900

    passed a concat-keys

--- question/concat-keys.js
+++ question/concat-keys.js
@@ -19,6 +19,59 @@
   @returns {Array}
 */
 
-export default function(array) {
+const objectArrayKeys = arr => {
+    let ret = [];
 
+    arr.forEach(elm => {
+        ret = ret.concat(Object.keys(elm));
+    });
+
+    ret = ret.filter((elm, index, self) => {
+        return self.indexOf(elm) === index;
+    });
+
+    return ret;
+};
+
+const getValues = elm => {
+    let ret = [];
+
+    if (typeof elm === 'object') {
+        let keys = Object.keys(elm);
+
+        keys.forEach(key => {
+            ret.push(elm[key]);
+        });
+    } else {
+        ret.push(elm);
+    }
+
+    return ret;
+};
+
+const findValues = (key, objArr) => {
+    let ret = [];
+
+    objArr.forEach(obj => {
+        if (obj.hasOwnProperty(key)) {
+            ret = ret.concat(getValues(obj[key]));
+        }
+    });
+
+    ret = ret.filter((elm, index, self) => {
+        return self.indexOf(elm) === index;
+    });
+
+    return ret.sort();
+};
+
+export default function(array) {
+    let tempObj = {};
+    const keys = objectArrayKeys(array);
+
+    keys.forEach((elm, index) => {
+        tempObj[keys[index]] = findValues(keys[index], array);
+    });
+
+    return [tempObj];
 }
